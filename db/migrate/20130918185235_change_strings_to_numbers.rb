21a1b77f3379589eef1965177f4553e9d9aecdd5
James Torio <jamestorio@yahoo.com>
2013-09-26 12:28:50 -0400

    changed time column to integer

--- db/migrate/20130918185235_change_strings_to_numbers.rb
+++ db/migrate/20130918185235_change_strings_to_numbers.rb
@@ -1,7 +1,8 @@
 class ChangeStringsToNumbers < ActiveRecord::Migration
   def change
     change_table :recents do |t|
-      t.change :time, :integer 
+      t.remove_column :time 
+      t.add_column :time, :integer 
       t.change :distance, :decimal
       t.change :average, :decimal
       t.change :speed, :decimal
